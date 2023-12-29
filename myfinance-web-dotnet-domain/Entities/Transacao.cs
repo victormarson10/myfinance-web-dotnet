3a446e81e996b3911025eb1320f59cbee9fdd620
victor10 <victormarson10@gmail.com>
2023-12-29 14:18:54 -0300

    Configuração EntityFramework

---
+++ myfinance-web-dotnet-domain/Entities/Transacao.cs
@@ -0,0 +1,12 @@
+﻿namespace myfinance_web_dotnet_domain.Entities;
+
+public class Transacao
+{
+    public int? Id { get; set; }
+    public string Historico { get; set; }
+    public string DateTime { get; set; }
+    public decimal valor { get; set; }
+    public int PlanoContaId { get; set; }
+    public PlanoConta planoConta { get; set; } 
+    
+}
---
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain.Entities;
+
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+    public DbSet<PlanoConta> PlanoConta { get; set; }
+
+    public DbSet<Transacao> Transacao { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=myfinance;Trusted_Connection=True;");
+    }
+
+}
