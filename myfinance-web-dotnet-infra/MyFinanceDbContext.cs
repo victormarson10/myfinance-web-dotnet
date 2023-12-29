2f9c2855dc962fec7418327c3a60bdf46c1be943
victor10 <victormarson10@gmail.com>
2023-12-29 18:49:38 -0300

    Criação tela Plano de Contas + correção conexão com banco

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -11,7 +11,7 @@ public class MyFinanceDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=localhost;Database=myfinance;Trusted_Connection=True;");
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False");
     }
 
 }
