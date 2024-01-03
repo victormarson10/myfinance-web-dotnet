2203b967d956794bfbb4d6c4eeb955e322f5ef67
victor10 <victormarson10@gmail.com>
2024-01-03 20:28:12 -0300

    Refatoração retirada string de conexão dbContext

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public DbSet<PlanoConta> PlanoConta { get; set; }
 
     public DbSet<Transacao> Transacao { get; set; }
+    public MyFinanceDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=localhost;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False");
+        var connectionString = _configuration.GetConnectionString("Database");
+        
+        optionsBuilder.UseSqlServer(connectionString);
     }
 
 }
